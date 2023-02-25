c4a31bb95fa10122879dbcbdbfa02a475e12492c
Fernando Costa <ferndcosta353@gmail.com>
2023-02-25 12:35:38 -0300

    Creating const variables to the elements I need to manipulate

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+const unitInputEl = document.getElementById("unit-input");
+const unitConverterButtonEl = document.getElementById("unit-converter-button");
+const lenghtResultEl = document.getElementById("lenght-result");
+const volumeResultEl = document.getElementById("volume-result");
+const massResult = document.getElementById("mass-result");
