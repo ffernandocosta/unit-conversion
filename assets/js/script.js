335e6366750b462f79a28f1b34a5694fee78f670
Fernando Costa <ferndcosta353@gmail.com>
2023-02-25 14:30:08 -0300

    created variables for the conversion numbers

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,7 @@ const unitConverterButtonEl = document.getElementById("unit-converter-button");
 const lenghtResultEl = document.getElementById("lenght-result");
 const volumeResultEl = document.getElementById("volume-result");
 const massResult = document.getElementById("mass-result");
+
+const meterToFeet =  3.281;
+const literToGallon =  0.264;
+const kiloToPound =  2.204;
