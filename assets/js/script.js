43dbe6d3438c225d7a7d06accadd437201375c91
Fernando Costa <ferndcosta353@gmail.com>
2023-02-25 14:49:23 -0300

    Added event listener and render function for unit conversion
    This feature renders it out the converted results when the user clicks convert passing a number on the unitInputEl field

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,21 @@ const massResult = document.getElementById("mass-result");
 const meterToFeet =  3.281;
 const literToGallon =  0.264;
 const kiloToPound =  2.204;
+
+unitConverterButtonEl.addEventListener("click", (e) => {
+    e.preventDefault();
+    render();
+});
+
+function render() {
+    let baseValue = unitInputEl.value;
+    
+    lenghtResultEl.textContent = `${baseValue} meters = ${(baseValue * meterToFeet).toFixed(3)} feet | ${baseValue} feet = ${(baseValue / meterToFeet).toFixed(3)}`;
+
+    volumeResultEl.textContent = `${baseValue} liters = ${(baseValue * literToGallon).toFixed(3)} gallons | ${baseValue} gallons = ${(baseValue / literToGallon).toFixed(3)}`
+    
+    massResult.textContent = `${baseValue} kilos = ${(baseValue * kiloToPound).toFixed(3)} pounds | ${baseValue} pounds = ${(baseValue / kiloToPound).toFixed(3)}`
+
+
+};
+
